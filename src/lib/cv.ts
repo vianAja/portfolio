926c2748342e2216858dbe8900b3473c56fcb13e
vian <vian@users.noreply.github.com>
2026-06-07 16:07:09 +0700

    Create local static portfolio page

--- src/lib/cv.ts
+++
@@ -1,8 +0,0 @@
-export const cvMeta = {
-  title: "Salinan CV - Najwan Octavian Gerrard.pdf",
-  fileId: "1b4WKdXl-JlTiUo5HERdd3x__yl5gI8O_",
-};
-
-export const cvPreviewUrl = `https://drive.google.com/file/d/${cvMeta.fileId}/preview`;
-export const cvViewUrl = `https://drive.google.com/file/d/${cvMeta.fileId}/view`;
-export const cvDownloadUrl = `https://drive.google.com/uc?export=download&id=${cvMeta.fileId}`;
